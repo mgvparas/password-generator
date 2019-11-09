6a5d87256f1a6762ebcded02652c905a911e397a
Gab Paras <miguel.gp@stationfive.com>
2019-11-10 01:16:57 +0800

    Create GeneratePassword.sh

---
+++ GeneratePassword.sh
@@ -0,0 +1,26 @@
+﻿#!/bin/bash
+echo ""
+echo "============================"
+echo "= Generate Base64 Password ="
+echo "============================"
+echo ""
+echo "Password length: "
+read PASSWORD_LENGTH
+
+ATTEMPT_COUNT=1
+REPEAT=1
+
+while [ "$REPEAT" == 1 ]
+do
+	echo "attempt $ATTEMPT_COUNT..."
+
+	generatedPassword=$(openssl rand -base64 48 | cut -c1-$PASSWORD_LENGTH)
+
+	if [[ $generatedPassword == *['!'@#\$%^\&*()_+]* ]]
+	then
+		echo "$generatedPassword"
+		REPEAT=0
+	else
+		((ATTEMPT_COUNT++))
+	fi
+done
